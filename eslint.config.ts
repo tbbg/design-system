1009774dc2f8a07e68cc8545eb7a5b8393ee4d83
resurrectdev <resurrectdev@rpgroot.com>
2025-07-13 23:30:56 +0200

    build: setup eslint

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,28 +1,28 @@
-import pluginVue from 'eslint-plugin-vue'
-import vueTsEslintConfig from '@vue/eslint-config-typescript'
+import pluginVue from "eslint-plugin-vue";
+import vueTsEslintConfig from "@vue/eslint-config-typescript";
 
 export default [
-  ...pluginVue.configs['flat/essential'],
+  ...pluginVue.configs["flat/essential"],
   ...vueTsEslintConfig(),
 
   {
     ignores: [
-      'node_modules/**',
-      'dist/**',
-      'coverage/**',
-      'storybook-static/**'
-    ]
+      "node_modules/**",
+      "dist/**",
+      "coverage/**",
+      "storybook-static/**",
+    ],
   },
 
   {
     rules: {
-      'vue/multi-word-component-names': 'error',
-      'vue/component-name-in-template-casing': ['error', 'PascalCase'],
-      'vue/component-definition-name-casing': ['error', 'PascalCase'],
-      '@```escript-eslint/no-explicit```y': 'error',
-      '@typescript-eslint/no```used-vars': 'error',
-      'no-console':```ocess.env.NODE_ENV === 'production' ? 'error``` 'warn',
-      'no-debugger': process.```.NODE_ENV === 'production' ? 'error' :```arn'
-    }
-  }
-]
+      "vue/multi-word-component-names": "warn",
+      "vue/component-name-in-template-casing": ["error", "PascalCase"],
+      "vue/component-definition-name-casing": ["error", "PascalCase"],
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "no-console": process.env.NODE_ENV === "production" ? "error" : "warn",
+      "no-debugger": process.env.NODE_ENV === "production" ? "error" : "warn",
+    },
+  },
+];
