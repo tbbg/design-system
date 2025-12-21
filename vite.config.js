e7350b10087446f567f4cbe2dfd739b6319ec15e
resurrectdev <resurrectdev@rpgroot.com>
2025-12-21 23:48:39 +0100

    build: rename design system

--- vite.config.js
+++ vite.config.js
@@ -17,8 +17,9 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
-      name: "MyDesignSystem",
-      fileName: (format) => `my-design-system.${format}.js`,
+      name: "rpgroot",
+      formats: ["es"], // avoid UMD/IIFE
+      fileName: () => "index.js",
     },
     rollupOptions: {
       external: ["vue"],
